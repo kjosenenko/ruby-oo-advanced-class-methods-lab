5b5f978fa8d82b6ebd866ba77105ddb5b1c16799
KJ Osenenko <karljosenenko@gmail.com>
2020-11-19 11:36:25 -0700

    commit tree 24b2ceb7e65af11290bdaa1ac19af694c29dc147

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,61 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    song = self.new
+    @@all<<song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all<<song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    new_song = filename.split(" - ")
+    song = self.new
+    song.name = new_song[1].gsub(".mp3", "")
+    song.artist_name = new_song[0]
+    song
+    # binding.pry
+  end
+
+  def self.create_from_filename(filename)
+    new_song = filename.split(" - ")
+    song = self.new
+    song.name = new_song[1].gsub(".mp3", "")
+    song.artist_name = new_song[0]
+    @@all<<song
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 
   def self.all
     @@all
